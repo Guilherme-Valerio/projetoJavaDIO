caff85a7136bae3c77a51de409f02c7e80023405
Guilherme da Silva Valerio <guilherme.svalerio@hotmail.com>
2024-07-08 14:32:58 -0300

    first commiit

---
+++ primeiro-projeto-dio-java/src/App.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        int anoAtual = 2024;
+        int ANO_NASCIMENTO = 2005;
+        final String BR = "Brasil";
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello, World!");
+        System.out.println("Nosso pais é o " + BR);
+
+        System.out.println("Qual seu primeiro nome?");
+        String nome1 = scanner.nextLine();
+
+        System.out.println("Qual seu segundo nome?");
+        String nome2 = scanner.nextLine();
+
+        String nomeCompleto = nomeCompleto(nome1, nome2);
+        System.out.println(nomeCompleto);
+        int soma = somar(anoAtual, ANO_NASCIMENTO);
+        System.out.println(soma);
+    }
+
+    public static int somar(int n1, int n2) {
+        return n1 + n2;
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
