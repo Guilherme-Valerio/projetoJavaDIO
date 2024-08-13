eb4ff519845050e2320d184ff8bda86b45d919e0
Guilherme da Silva Valerio <guilherme.svalerio@hotmail.com>
2024-08-13 10:20:06 -0300

    iphone interface
    create project using interface and objetcs

---
+++ exerciocios-dio-java/src/iphone/aparelho/Iphone.java
@@ -0,0 +1,54 @@
+package iphone.aparelho;
+
+import iphone.aplicativos.AparelhoTelefonico;
+import iphone.aplicativos.NavegadorInternet;
+import iphone.aplicativos.ReprodutorMusical;
+
+public class Iphone implements NavegadorInternet, AparelhoTelefonico, ReprodutorMusical{
+
+    @Override
+    public void tocar() {
+        System.out.println("Reproduzindo música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Música selecionada: " + musica);
+    }
+
+    @Override
+    public void ligar() {
+        System.out.println("Ligando...");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada...");
+    }
+
+    @Override
+    public void iniciarCorreioDeVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+
+}
